24066e40fa94b015da093f18f22ac31ea40c033e
alamnurcahaya <alamnurcahaya@localhost>
2022-10-18 17:39:11 +0700

    V1.6: Menambahkan persistence dan konstruktor Pegawai

--- src/main/java/migratedb/senamjantung/PegawaiJpaController.java
+++ src/main/java/migratedb/senamjantung/PegawaiJpaController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import migratedb.senamjantung.exceptions.NonexistentEntityException;
 import migratedb.senamjantung.exceptions.PreexistingEntityException;
 
@@ -26,7 +27,10 @@ public class PegawaiJpaController implements Serializable {
     public PegawaiJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("migratedb_senamjantung_jar_0.0.1-SNAPSHOTPU");
+
+    public PegawaiJpaController() {
+    }
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
